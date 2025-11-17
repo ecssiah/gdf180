812ddf7c219abce1de57a699e6fae9330c3ba54d
ecssiah <ecssiah@gmail.com>
2025-11-17 21:50:35 +0800

    update terrain defaults

--- Source/GDF180/Private/Data/TerrainConfig.cpp
+++ Source/GDF180/Private/Data/TerrainConfig.cpp
@@ -3,15 +3,40 @@
 UTerrainConfig::UTerrainConfig()
 	:
 	Seed { 813 },
-	CellSizeInCentimeters { 100.0f },
+	CellSizeInCentimeters { 200.0f },
 	SectorSizeInCells { 20 },
-	WorldSizeInSectors { 12 },
+	WorldSizeInSectors { 8 },
 	WaterLevel { 0.0f },
 	NoiseGroupArray { TArray<FNoiseGroup>() },
 	bDebugBiomes { false },
-	BiomePeriod { 100.0f },
+	BiomePeriod { 10000.0f },
 	BiomeDefinitionArray { TArray<FBiomeDefinition>() }
 {
+	FNoiseGroup TerrainNoiseGroup;
+	TerrainNoiseGroup.Name = TEXT("Terrain");
+	TerrainNoiseGroup.NoiseLayerArray = TArray<FNoiseLayer>();
+	
+	FNoiseLayer TerrainNoiseLayer0;
+	TerrainNoiseLayer0.Weight = 1.0f;
+	TerrainNoiseLayer0.Period = 5000.0f;
+	TerrainNoiseLayer0.Amplitude = 1000.0f;
+		
+	TerrainNoiseGroup.NoiseLayerArray.Add(TerrainNoiseLayer0);
+		
+	FNoiseGroup WaterNoiseGroup;
+	WaterNoiseGroup.Name = TEXT("Water");
+	WaterNoiseGroup.NoiseLayerArray = TArray<FNoiseLayer>();
+	
+	FNoiseLayer WaterNoiseLayer0;
+	WaterNoiseLayer0.Weight = 1.0f;
+	WaterNoiseLayer0.Period = 100.0f;
+	WaterNoiseLayer0.Amplitude = 20.0f;
+	
+	WaterNoiseGroup.NoiseLayerArray.Add(WaterNoiseLayer0);
+
+	NoiseGroupArray.Add(TerrainNoiseGroup);
+	NoiseGroupArray.Add(WaterNoiseGroup);
+	
 	FBiomeDefinition GrassBiomeDefinition;
 	GrassBiomeDefinition.Name = TEXT("Grass");
 	GrassBiomeDefinition.DebugColor = { 0.0f, 1.0f, 0.0f, 1.0f };
@@ -27,37 +52,6 @@ UTerrainConfig::UTerrainConfig()
 	BiomeDefinitionArray.Add(GrassBiomeDefinition);
 	BiomeDefinitionArray.Add(SandBiomeDefinition);
 	BiomeDefinitionArray.Add(RockBiomeDefinition);
-		
-	FNoiseGroup TerrainNoiseGroup;
-	TerrainNoiseGroup.Name = TEXT("Terrain");
-	TerrainNoiseGroup.NoiseLayerArray = TArray<FNoiseLayer>();
-	
-	FNoiseLayer TerrainNoiseLayer0;
-	TerrainNoiseLayer0.Weight = 1.0f;
-	TerrainNoiseLayer0.Period = 200.0f;
-	TerrainNoiseLayer0.Amplitude = 2000.0f;
-		
-	FNoiseLayer TerrainNoiseLayer1;
-	TerrainNoiseLayer1.Weight = 1.0f;
-	TerrainNoiseLayer1.Period = 100.0f;
-	TerrainNoiseLayer1.Amplitude = 100.0f;
-	
-	TerrainNoiseGroup.NoiseLayerArray.Add(TerrainNoiseLayer0);
-	TerrainNoiseGroup.NoiseLayerArray.Add(TerrainNoiseLayer1);
-		
-	FNoiseGroup WaterNoiseGroup;
-	WaterNoiseGroup.Name = TEXT("Water");
-	WaterNoiseGroup.NoiseLayerArray = TArray<FNoiseLayer>();
-	
-	FNoiseLayer WaterNoiseLayer0;
-	WaterNoiseLayer0.Weight = 1.0f;
-	WaterNoiseLayer0.Period = 100.0f;
-	WaterNoiseLayer0.Amplitude = 200.0f;
-	
-	WaterNoiseGroup.NoiseLayerArray.Add(WaterNoiseLayer0);
-
-	NoiseGroupArray.Add(TerrainNoiseGroup);
-	NoiseGroupArray.Add(WaterNoiseGroup);
 }
 
 float UTerrainConfig::GetSectorSizeInCentimeters() const
